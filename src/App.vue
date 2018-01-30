2426906808e0c7f52ddea94473c5a63ba7f35e14
Ivan Fadila Putra <i.fadilaputra@gmail.com>
2018-01-30 10:08:40 +0700

    First Blood

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <h1>Hello</h1>
+    </div>
+</template>
+
+<style lang="css">
+    #app {
+      color: #56b983;
+    }
+</style>
+
+<script>
+    export default {
+        name : 'app',
+    }
+</script>
+
